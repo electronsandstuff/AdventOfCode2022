e3ad6d7debadf16bf914ba6625400a517e3e352f
Christopher M. Pierce <chris@chris-pierce.com>
2022-12-07 15:34:42 -0800

    big speed bump in parsing

--- src/day07.jl
+++ src/day07.jl
@@ -3,33 +3,46 @@ module Day07
 using AdventOfCode2022
 
 struct FSNode
+    name::String
+    parent::Union{FSNode, Nothing}
     files::Vector{Tuple{String, Int}}
-    dirs::Dict{String, FSNode}
-end
-FSNode() = FSNode(Vector{Tuple{String, Int}}(), Dict{String, FSNode}())
-
-pathsep = Sys.iswindows() ? '\\' : '/'
-get_files(fs, path::AbstractString) = get_files(fs, split(path, pathsep, keepempty=false))
-function get_files(fs, path::Vector)
-    isempty(path) ? fs.files : get_files(get!(fs.dirs, first(path), FSNode()), path[2:end])
+    dirs::Vector{FSNode}
 end
 
 line2file(l) = let n=findfirst(' ', l); (l[n+1:end], parse(Int, l[1:n-1])); end
 files(s) = line2file.(filter(x->!isempty(x)&&isnumeric(x[1]), split(s, '\n')))
 
 function assemble_fs(s)
-    path = ""
-    fs = FSNode()
+    cwd = fs = FSNode("/", nothing, [], [])
     for chunk in eachsplit(s, raw"$ cd ", keepempty=false)
         n=findfirst('\n', chunk)
-        path = normpath(joinpath(path, chunk[1:n-1]))
-        n != length(chunk) && append!(get_files(fs, path), files(chunk[n:end]))
+        dir = chunk[1:n-1]
+        if dir == "/"; cwd = fs
+        elseif dir == ".."; cwd = cwd.parent
+        else
+            idx = findfirst(x->x.name==dir, cwd.dirs)
+            if idx === nothing
+                cwd = push!(
+                    cwd.dirs, 
+                    FSNode(dir, cwd, Vector{Tuple{String, Int}}(), Vector{FSNode}())
+                    ) |> last
+            else; cwd = cwd.dirs[idx]
+            end
+        end
+        n != length(chunk) && append!(cwd.files, files(chunk[n:end]))
     end
     fs
 end
 
 dirsizes(fs::FSNode)::Vector{Int} = let s = dirsizes.(values(fs.dirs))
     push!(vcat(s...), sum(last.(fs.files)) + sum(last.(s)))
+end
+
+function printfs(fs, name="/", indent=0)
+    start = repeat(" ", 2*indent)
+    println(start, "- ", name, " (dir)")
+    [printfs(d, d.name, indent+1) for d in fs.dirs]
+    [println(start, "  - ", f[1], " (file, size=", f[2], ")") for f in fs.files]
 end
 
 function day07(input::String = readInput(joinpath(@__DIR__, "data", "day07.txt")))
